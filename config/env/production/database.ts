df4c313bcaec5084cf11032d8062ea424d65fa16
Abdullah0Adel <abdullahadel365@gmail.com>
2026-07-11 04:35:48 +0300

    edit database.ts

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,15 +1,13 @@
 export default ({ env }: any) => ({
   connection: {
-    client: 'postgres',
+    client: env('DATABASE_CLIENT', 'postgres'),
     connection: {
       host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: {
-        rejectUnauthorized: false
-      },
+      ssl: { rejectUnauthorized: false },
     },
     pool: { min: 0, max: 10 }
   },
